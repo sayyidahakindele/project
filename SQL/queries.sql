cffd354f321f73d2839e67bfa73484f4503b7aec
sayyidahakindele <akindelesayyidah@gmail.com>
2023-12-11 02:28:23 -0500

    query updates

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -17,18 +17,26 @@ LEFT JOIN routines ON members.member_id = routines.member_id
 LEFT JOIN exercises ON routines.routine_id = exercises.routine_id
 WHERE users.username = 'damibisi' AND routines.routine_description = 'Arm morning workout';
 
--- -- Her email got hacked so she had to make a new one. She decides to update her email on all her
--- -- accounts  ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- -- Her email got hacked so she had to make a new one. She decides to update her email on all her --
+-- -- accounts  --------------------------------------------------------------------------------------
 UPDATE users SET email = 'newdamilolaola@example.com' WHERE user_id = (SELECT user_id from users WHERE username = 'damibisi');
---select
+SELECT * FROM users WHERE user_id = (SELECT user_id from users WHERE username = 'damibisi');
+
 -- -- A new member decides to join the fitness club. They create an account as a member.
 INSERT INTO users (type_of_user, username, password_key, first_name, last_name, email, phone_num, sex, dob, home_addr) VALUES ((SELECT user_type_id from user_types WHERE user_type_name = 'member'), 'kellygetsfit', 'kellyspassword', 'Kelly', 'McQueen', 'queenkelly@example.com', '9812562749', 'female', '1983-11-19', '43B Hampton Drive, Toronto, Ontario, Canada');
 INSERT INTO members(member_id) VALUES ((SELECT user_id from users WHERE username = 'kellygetsfit'));
 
+SELECT * FROM users ORDER BY user_id DESC LIMIT 1;
+
 -- -- She is prompted to enter her health metrics
 INSERT INTO metrics(member_id, new_weight, height) VALUES ((SELECT user_id from users WHERE username = 'kellygetsfit'), 186, 150);
 
--- select
+SELECT users.username, metrics.*
+FROM metrics
+JOIN members ON metrics.member_id = members.member_id
+JOIN users ON members.member_id = users.user_id
+WHERE users.username = 'damibisi';
+
 -- The fitness club hired a new trainer. Admin creates a new profile for them.
 INSERT INTO users (type_of_user, username, password_key, first_name, last_name, email, phone_num, sex, dob, home_addr) VALUES ((SELECT user_type_id from user_types WHERE user_type_name = 'trainer'), 'AishatA', 'aishaspassword', 'Aishat', 'Adesina', 'aoadesina@example.com', '8793789132', 'female', '1980-11-14', '124 Foxtrot Block, Fruter Street, Ottawa, Ontario, Canada');
 INSERT INTO trainers(trainer_id) VALUES ((SELECT user_id from users WHERE username = 'AishatA'));
@@ -37,7 +45,12 @@ INSERT INTO certification_types (certification_name, trainer_id) VALUES ('cardio
 INSERT INTO certification_types (certification_name, trainer_id) VALUES ('yoga', (SELECT user_id from users WHERE username = 'AishatA'));
 INSERT INTO certification_types (certification_name, trainer_id) VALUES ('zumba', (SELECT user_id from users WHERE username = 'AishatA'));
 
---
+SELECT users.*, certification_types.certification_name
+FROM users
+JOIN trainers ON users.user_id = trainers.trainer_id
+LEFT JOIN certification_types ON trainers.trainer_id = certification_types.trainer_id
+WHERE users.username = 'aorogat';
+
 -- -- Damilola wants a one-one yoga session. First she searches for trainers who are certified in yoga
 SELECT
     trainers.trainer_id,
